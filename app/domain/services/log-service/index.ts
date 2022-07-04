509a111a025cc7cb6cdb21c66fb91456aa0359d2
Dmytro Panontko <dmytro.panontko@dat.com>
2022-07-04 19:31:17 +0300

    fix: comments to PR

--- app/domain/services/log-service/index.ts
+++ app/domain/services/log-service/index.ts
@@ -7,9 +7,14 @@ const pinoOptions = {
   messageKey: 'message', // Log the string passed to a log method under this key (instead of the default `msg`)
   nestedKey: 'payload',  // Log the object passed to a log method under this key
   formatters: {
-    level(label: unknown) {
+    level(label: unknown /* , number: unknown */) {
+      // default is { level: number }
       return { level: label };
     },
+    // bindings(/* bindings: Record<string, unknown> */) {
+    //   // default is { pid, hostname }
+    //   return {};
+    // },
   },
   base: {} // the same effect as of (now commented out) bindings above
 };
