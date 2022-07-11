a677403e78745d6a9f60afd7eacff1f3c7eb9262
Serhii Ishchenko <serhii.ishchenko@planetpayment.com>
2022-07-11 19:16:24 +0300

    feat: Correct routing to (noop yet) webhook-notification handler

--- app/domain/environment-agnostic-handlers/per-operation-handlers/webhook-notification/handler.ts
+++ app/domain/environment-agnostic-handlers/per-operation-handlers/webhook-notification/handler.ts
@@ -6,16 +6,17 @@ import { HttpStatusCode } from 'http-status-code-const-enum';
 import { RequestBodySchemaType } from './request-schema';
 import logger from '../../../services/log-service';
 // import { PaymentService } from '../../../services/payment-service/service';
-import { DatatransService } from '../../../services/datatrans-service';
+// import { DatatransService } from '../../../services/datatrans-service';
 
 export default async (req: IAbstractRequestWithTypedBody<RequestBodySchemaType>): Promise<IAbstractResponse> => {
   logger.debug(req, 'Request from Datatrans');
 
-  const rawRequestBody = JSON.stringify(req.body); // TODO: think about JSON.stringify...
+  // const rawRequestBody = JSON.stringify(req.body);
+  // At a higher level the raw request body was already a string - but that would look strange to pass it to here - so JSON.stringify it again...
 
   /// Validate the signature of the received notification
   // try {
-  DatatransService.validateIncomingRequestSignature(req.body.merchantId, req.headers, rawRequestBody);
+  // DatatransService.validateIncomingRequestSignature(req.body.merchantId, req.headers, rawRequestBody);
   // } catch (err) {
 
   // }
