7178b797ed7bf418fca35665c02c411042100159
Fabio Carvalho <fabio.carvalho@planetpayment.com>
2022-07-27 15:17:50 +0100

    INC-5 - fixed typos, added comments, clarified messages

--- deploy/commercetools/ct-setup.sh
+++ deploy/commercetools/ct-setup.sh
@@ -9,15 +9,24 @@
 # Each file represents a custom field type.
 
 # REQUIREMENTS:
-# The following variables must be obtained from CommerceTools project setup
-# (usually after the API Client creation)
-# You can get some insights about the variables semantics in README.md at this repository root.
-CT_API_URL=""
-CT_AUTH_URL=""
+# - "jq" installed in the shell/os to be used
+# - The following variables must be obtained from CommerceTools project API client setup
+#   You can get some insights about the variables semantics in README.md at this repository root.
+#
+CT_API_URL="" # !!! remove any trailing slash
+CT_AUTH_URL="" # !!! remove any trailing slash
 CT_PROJECT_ID=""
 CT_CLIENT_ID=""
 CT_CLIENT_SECRET=""
 CTP_SCOPES=""
+#
+echo "##### Starting...."
+#
+ACCESS_TOKEN=$(curl ${CT_AUTH_URL}/oauth/token --silent \
+     --basic --user "${CT_CLIENT_ID}:${CT_CLIENT_SECRET}" \
+     -X POST \
+     -d "grant_type=client_credentials&scope=${CTP_SCOPES}" |\
+     jq -r '.access_token')
 
 # An access token example
 #{
@@ -26,17 +35,13 @@ CTP_SCOPES=""
 #	"expires_in":172800,
 #	"scope":"manage_project:planetpayment-discovery"
 #}
-ACCESS_TOKEN=$(curl ${CT_AUTH_URL}/oauth/token \
-     --basic --user "${CT_CLIENT_ID}:${CT_CLIENT_SECRET}" \
-     -X POST \
-     -d "grant_type=client_credentials&scope=${CTP_SCOPES}" |\
-     jq -r '.access_token')
 
-echo "##### Got an access token from commercetools: ${ACCESS_TOKEN}"
+echo "   ## Got an access token from CommerceTools: ${ACCESS_TOKEN}"
 
 for filename in ./types/*.json; do
+	echo " "
 	typeKey=$(basename "$filename" .json)
-	echo "##### Checking if ${typeKey} exists in commercetools..."
+	echo "##### Checking if '${typeKey}' type exists in CommerceTools..."
 
 	statusCode=$(curl --write-out '%{http_code}' --silent --output /dev/null \
 		-X GET ${CT_API_URL}/${CT_PROJECT_ID}/types/key=${typeKey} -i \
@@ -44,29 +49,32 @@ for filename in ./types/*.json; do
 
 	if [[ $statusCode -eq 404 ]]
 	then
-		echo "      Type ${typeKey} does not exists in commercetools. Creating..."
+		echo "   ## Type DOES NOT exists. Creating it..."
 
 		statusCode=$(curl --write-out '%{http_code}' --silent --output /dev/null \
-			-X POST ${CT_API_URL}/${CTP_PROJECT_KEY}/types -i \
+			-X POST ${CT_API_URL}/${CT_PROJECT_ID}/types -i \
 			--header "Authorization: Bearer ${ACCESS_TOKEN}" \
 			--header 'Content-Type: application/json' \
 			--data-binary "@${filename}")
 
 		if [[ $statusCode -eq 201 ]]
 		then
-			echo "      Type ${typeKey} successfully created!"
+			echo "   ## Type '${typeKey}' successfully created!"
 		else
-			echo "      Got HTTP ${statusCode}. Not expected!"
+			echo "   !! Got HTTP ${statusCode} when creating type. Not expected!"
+			exit 1
 		fi
 
 	elif [[ $statusCode -eq 200 ]]
 	then
-		echo "   Type ${typeKey} does exists in commercetools. Skip it."
+		echo "   ## Type does exists, skipping its creation."
 	else
-		echo "   Got HTTP ${statusCode}. Not expected."
+		echo "   !! Oh no, got an HTTP ${statusCode}. Not expected."
+		exit 1
 	fi
 
 done
+echo " "
 echo "##### Done."
 
 exit 0
