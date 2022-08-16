3958d4aebc38a6044bb7500da0df545cd518eff1
Artur Chystiak <artur_chystiak@epam.com>
2022-08-16 17:14:46 +0300

    INC-123 - change hanlder path in deploy scripts

--- app/environment-specific-handlers/aws-lambda-via-arn/index.ts
+++ app/environment-specific-handlers/aws-lambda-via-arn/index.ts
@@ -1,4 +1,4 @@
-import { AwsArnGatewayAdapter } from './api-gateway-adapter';
+import { AwsArnGatewayAdapter } from './aws-lambda-arn-adapter';
 import { allOperationHandler as envAgnosticAllOpsHandler }  from '../../domain/environment-agnostic-handlers';
 
 const awsApiGatewayAdapter = new AwsArnGatewayAdapter();
