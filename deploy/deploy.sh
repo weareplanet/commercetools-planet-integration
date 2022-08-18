5af9cb5b23851f68b301042e6dc521154c9aeb19
Paul Bourke <pauldbourke@protonmail.com>
2022-08-18 11:37:30 +0000

    Refactoring

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,6 +4,14 @@ set -euo pipefail
 
 DEPLOY_SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
+SUPPORTED_ENVS="aws"
+DEFAULT_STACK_ID="connector"
+
+ENV="${ENV:-""}"
+ENV_FILE="${ENV_FILE:-${DEPLOY_SCRIPT_DIR}/env}"
+STACK_ID="${STACK_ID:-${DEFAULT_STACK_ID}}"
+AWS_REGION="${AWS_REGION:-eu-west-1}"
+
 function deploy_aws {
   source ${ENV_FILE}
   source ${DEPLOY_SCRIPT_DIR}/commercetools/custom-types-setup.sh
@@ -15,40 +23,35 @@ function deploy_aws {
 }
 
 function usage {
-	cat << EOF
+  cat << EOF
 
 Deploys the Planet Payment commercetools connector.
 
 Usage: $0 -e environment [-s stack_id]
 
 Arguments:
--e Target environment, one of: 'aws'
--s (Optional) Name of the infra stack created. Default: 'connector'
+-e Target environment, one of: '${SUPPORTED_ENVS}'
+-s (Optional) Name of the infra stack created. Default: '${DEFAULT_STACK_ID}'
 EOF
 }
 
-ENV="${ENV:-""}"
-ENV_FILE="${ENV_FILE:-${DEPLOY_SCRIPT_DIR}/env}"
-STACK_ID="${STACK_ID:-resources}"
-AWS_REGION="${AWS_REGION:-eu-west-1}"
-
 while getopts "e:s:" OPTION; do
-    case $OPTION in
+  case $OPTION in
     e)
-        ENV=$OPTARG
-        if [[ ! $ENV =~ aws ]]; then
-          usage
-          exit 1
-        fi
-        ;;
-    s)
-        STACK_ID=$OPTARG
-        ;;
-    *)
+      ENV=$OPTARG
+      if [[ ! $ENV =~ ${SUPPORTED_ENVS[@]} ]]; then
         usage
         exit 1
-        ;;
-    esac
+      fi
+      ;;
+    s)
+      STACK_ID=$OPTARG
+      ;;
+    *)
+      usage
+      exit 1
+      ;;
+  esac
 done
 
 case $ENV in
