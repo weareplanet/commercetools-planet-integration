32595d2ff9f3a13acfb12a46161944f0daac01af
Nisha Chaganlal <Nisha.Chaganlal@planetpayment.com>
2022-09-12 15:28:05 +0100

    Addressing Major vulnerability detected by sonar https://sonarcloud.io/project/issues?branch=production-rediness&issues=AYMyEcijabaGYY-ur4Wn&open=AYMyEcijabaGYY-ur4Wn&id=commerceTools

--- app/interfaces/commerce-tools/index.ts
+++ app/interfaces/commerce-tools/index.ts
@@ -1,7 +1,6 @@
 export * from './payment';
 export * from './interface-interaction';
 export * from './custom-type-keys';
-// export * from './request-to-ct';
 export * from './request-from-ct';
 export * from './error';
 export * from './error-messages';
